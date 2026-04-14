3aa30c60beeb74518fd1bca8f526da34d6f3e8da
ZuriKo <ZuriKo83@users.noreply.github.com>
2026-04-15 08:05:20 +0900

    Clear repository

--- run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-start "Host Bridge" cmd /k call "%cd%\scripts\run_host_bridge.bat"
-docker compose up --build
-pause
--- scripts/check_vmware.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-if "%VMRUN_PATH%"=="" set VMRUN_PATH=C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe
-if not exist "%VMRUN_PATH%" (
-  echo vmrun.exe not found: %VMRUN_PATH%
-  exit /b 1
-)
-"%VMRUN_PATH%" list
